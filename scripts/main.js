351f6c8cafb18191867aac11083f3d42f78f164d
gyokochen <71756843+gyokochen@users.noreply.github.com>
2022-03-23 13:42:34 +0100

    Add files via upload
    first try

---
+++ scripts/main.js
@@ -0,0 +1,67 @@
+
+let myImage = document.querySelector('img');
+myImage.onclick = function() 
+{
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/letter.jpeg') 
+    {
+      myImage.setAttribute('src','images/paint.jpeg');
+    } 
+    else 
+    {
+      myImage.setAttribute('src','images/letter.jpeg');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() 
+{
+    let myName = prompt('Please enter your name.');
+    if(!myName) 
+    {
+        setUserName();
+    } 
+    else 
+    {
+        localStorage.setItem('name', myName);
+        myHeading.textContent = 'Mozilla is cool, ' + myName;
+    }
+    
+}
+//function to ask for fulfilling the information
+
+if(!localStorage.getItem('name')) 
+{
+    setUserName();
+} 
+else 
+{
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+//first loading
+
+
+myButton.onclick = function() 
+{
+    setUserName();
+}
+//click to run the function
+
+
+
+
+
+
+/*
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+
+document.querySelector('html').addEventListener('click', function() {
+    alert('Ouch! Stop poking me!');
+  });
+
+*/
